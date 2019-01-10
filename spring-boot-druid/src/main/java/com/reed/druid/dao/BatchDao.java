6243624f2a7ab238aa2ccf188788c17e6adb3d4f
reedF <fym_82@163.com>
2019-01-10 15:41:18 +0800

    comment

--- spring-boot-druid/src/main/java/com/reed/druid/dao/BatchDao.java
+++ spring-boot-druid/src/main/java/com/reed/druid/dao/BatchDao.java
@@ -22,15 +22,17 @@ public class BatchDao {
 	public static final String TB_DB = "tb_user_info";
 
 	// 使用REPLACE INTO根据主键执行insert or update
-	// REPLACE INTO是对重复键先delete再insert，故对于存在多个唯一索引的表，在并发时易出现死锁,
+	// REPLACE INTO是对重复键先delete再insert，故对于存在多个唯一索引的表，在并发时易出现死锁,且如果存在多个唯一性索引时，一次replace语句，可能导致多条记录被删除
 	// https://siyong.sinaapp.com/2018/08/27/%E5%B9%B6%E5%8F%91replace-into%E5%AF%BC%E8%87%B4mysql%E6%AD%BB%E9%94%81/
+	// http://www.xuchunyang.com/2016/01/24/replace_duplicate_upd_different/
 	public static final String SQL_INSERT_TB_DB =
 			//
 			"REPLACE INTO tb_user_info(id,name,createTime,updateTime) "
 					//
 					+ " VALUES(:id,CONCAT('${PREFIX}',:name),:createTime,:updateTime) ";
 	// INSERT INTO ON DUPLICATE KEY UPDATE
-	// 如果一个表定义有多个唯一键或者主键时，并发时也易出现死锁
+	// 如果一个表定义有多个唯一键或者主键时，并发时也易出现死锁，
+	// 且使用此类语句时建议将所有字段都作为更新字段，避免只更新部分字段，因为此时如存在多个唯一索引时,有可能会在非主键上的唯一索引冲突导致的字段更新覆盖
 	// https://www.2cto.com/database/201711/695662.html
 	public static final String SQL_INSERT_ONDUPLICATEKEY_TB_DB =
 			//
